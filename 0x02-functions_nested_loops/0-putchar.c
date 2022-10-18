b051e083c0acd6c339a854859463b1a0b5deeff0
Jay-Rico <ericsotonye@gmail.com>
2022-10-18 12:32:29 -0700

    alx

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+/* The program will print "_putchar" and a new line */
+#include "main.h"
+/**
+ * main - program execution begins here
+ * Return: returns 0
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
